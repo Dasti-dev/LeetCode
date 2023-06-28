fa116df5784a3ff0f59b09bb666a8f668639d7db
Dasti-dev <117101772+Dasti-dev@users.noreply.github.com>
2023-06-29 02:05:07 +0530

    Create 210. Course Schedule II.cpp

---
+++ 210. Course Schedule II.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites) {
+        vector<int> adj[N];
+        for(auto it : prerequisites)
+        {
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        vector<int> indeg(N,0);
+        for(int i = 0 ; i < N ; i++)
+        {
+            for(auto it : adj[i]){
+                indeg[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0 ; i < indeg.size() ; i++)
+        {
+            if(indeg[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int> sol;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            sol.push_back(node);
+            for(auto it : adj[node])
+            {
+                indeg[it]--;
+                if(indeg[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        if(sol.size() == N)
+        {
+            reverse(sol.begin(),sol.end());
+            return sol;
+        }
+        else
+        {
+            return vector<int>(0);
+        }
+    }
+};
